7cc9b373e8e258d801d179fdb3b630f5b8aacbd5
Tai Bui <taibui97@outlook.com>
2026-03-01 23:02:28 -0800

    database config

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -3,15 +3,24 @@ import { PrismaPg } from '@prisma/adapter-pg'
 import fs from 'fs'
 import path from 'path'
 
-const ca = fs.readFileSync(
-  path.join(process.cwd(), 'certs', 'ca.pem')
-)
-
+let sslConfig;
+if (process.env.RENDER) {
+    sslConfig = {
+        rejectUnauthorized: true,
+        ca: process.env.AIVEN_CA_CERT,
+    }
+}
+else {
+    sslConfig = {
+        rejectUnauthorized: true,
+        ca: fs.readFileSync(
+            path.resolve(process.cwd(), "certs/ca.pem")
+        ).toString()
+    }
+}
 const adapter = new PrismaPg({
     connectionString: process.env.DATABASE_URL!,
-    ssl: {
-        ca,
-    },
+    ssl: sslConfig,
 })
 
 const prisma = new PrismaClient({
